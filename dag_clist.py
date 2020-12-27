f96f91c42e882a67c14f4150298b9669d46c22d2
william-cass-wright <william.cass.wright@gmail.com>
2020-12-27 14:21:54 -0700

    scheduler stops running periodically and next dag execution doesn't seem to automatically populate

--- dag_clist.py
+++ dag_clist.py
@@ -2,28 +2,29 @@
 Author: William Wright
 '''
 
-from datetime import timedelta
+import datetime 
 
 from airflow import DAG
 
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 
-default_args = {
+args = {
     'owner': 'willcasswrig',
     'depends_on_past': False,
     'email': ['william.cass.wright@gmail.com'],
-    'email_on_failure': True,
-    'email_on_retry': True,
+    'email_on_failure': False,
+    'email_on_retry': False,
     'retries': 3,
-    'retry_delay': timedelta(minutes=1),
+    'retry_delay': datetime.timedelta(minutes=1),
+    'start_date':datetime.datetime(2020, 12, 1, 0, 0),
 }
 
 dag = DAG('dag_clist',
-          default_args=default_args,
+          default_args=args,
           description='grabs craigslist rental listings',
-          schedule_interval='0 */12 * * *',
-          start_date=days_ago(1))
+          schedule_interval=datetime.timedelta(0.5),
+          catchup=False)
 
 start_dag = BashOperator(
     task_id='print_date',
@@ -32,13 +33,17 @@ start_dag = BashOperator(
 )
 clist = BashOperator(
     task_id='clist',
-    bash_command='''python /home/pi/airflow/dags/src/main_clist.py''',
+    bash_command='''python /home/pi/src/main_clist.py''',
     dag=dag,
 )
+# template f'''email body
+# execution date:\t\t{{ ds }}
+# previous execution date:\t\t{{ prev_ds }}
+# next execution date:\t\t{{ next_ds }}'''
+
 email = BashOperator(
     task_id='email',
-    bash_command='''echo "sending email"
-        python /home/pi/airflow/dags/src/main_email.py''',
+    bash_command='''python /home/pi/src/main_email.py''',
     dag=dag,
 )
 
