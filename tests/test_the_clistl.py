0573faf53bfaf8e08ca23bf86f51fc2d0c2e6674
Kodya Morozov <motifreflectionpositive@gmail.com>
2023-01-24 02:52:18 +0200

    fix test

--- tests/test_the_clistl.py
+++ tests/test_the_clistl.py
@@ -1,12 +1,12 @@
 import sys
 sys.path.append('./src/')
 
-from main_email import mail
-from main_clist import collect_clist_data
+from src.module_clist.collect_clist import clean_clist_df
 import pandas as pd
-import smtplib
 
 # Test to check if dataframe is not empty
+
 def test_dataframe_not_empty():
-    df = collect_clist_data()
+    df = {'bedroom': [1], 'geotag_lat': [2], 'geotag_lon': [2], 'name': ["city"]}
+    df = clean_clist_df(df)
     assert not df.empty, "Dataframe is empty"
